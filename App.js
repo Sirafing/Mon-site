3ffdb3ef77b57d9eabfba4a77c7e7451ecda189d
Sirafing <sirakeith2422@gmail.com>
2025-06-17 09:21:29 +0000

    App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", paddingTop: "40px" }}>
+        Bienvenue sur Afrizone 🌍
+      </h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
